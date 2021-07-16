dbbc69032be72c8322c5a5f890e7f373e7b2f72d
ndanu-josy <josphinendanu3@gmail.com>
2021-07-16 21:15:23 +0300

    make migrations, add index

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from awwardsapp.forms import RegistrationForm
+from django.contrib.auth.models import User
+from django.http import request
+
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    
+
+    return render(request,'index.html')
+       
+@login_required(login_url='/accounts/login/')
+
+def profile(request):
+    
+
+    return render(request,'index.html')
+       
+
+def register(request):
+    if request.method=="POST":
+        form=RegistrationForm(request.POST)
+        
+        if form.is_valid():
+        
+                form.save()
+                username = form.cleaned_data.get('username')
+                raw_password = form.cleaned_data.get('password1')
+                user = authenticate(username=username, password=raw_password)
+                login(request, user)
+        return redirect('login')
+    else:
+        form= RegistrationForm()
+    return render(request, 'registration/registration_form.html', {"form":form}) 
+
