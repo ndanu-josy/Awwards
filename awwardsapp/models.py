038121046e78d303ff978bce7e1c3d8aae6b927b
ndanu-josy <josphinendanu3@gmail.com>
2021-07-16 22:07:57 +0300

    add project andprofile model

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class Profile(models.Model):
+    profile_picture = CloudinaryField('pic')
+    bio = models.CharField(max_length=50)  
+    projects = models.CharField(max_length=50)  
+    contact_info = models.CharField(max_length=60,blank=True)
+   
+class Project(models.Model):
+    title = models.CharField(max_length=60,blank=True)
+    image = CloudinaryField('img')
+    description = models.CharField(max_length=400)
+    link = models.URLField(blank=True)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile,null=True,on_delete=models.CASCADE)
+    
