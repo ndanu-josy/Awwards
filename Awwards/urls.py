a06d6eeb2daedfabe42fb84b087fa9462e2fc221
ndanu-josy <josphinendanu3@gmail.com>
2021-07-16 21:00:26 +0300

    Add urls and registration templates

--- Awwards/urls.py
+++ Awwards/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
+from django.contrib.auth import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'', include('awwardsapp.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.LogoutView.as_view(), {"next_page": '/'}),
 ]
